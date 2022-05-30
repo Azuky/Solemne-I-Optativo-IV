6a48a27964f85f39b07a48f63b4681c1cd6c6ad9
Azuki <m.gutierrez.tolorza@gmail.com>
2022-05-29 22:35:28 -0400

    + Se añade reporte + Modificaciones a scraping

--- src/scraping.js
+++ src/scraping.js
@@ -16,7 +16,7 @@ const scraping = async () => {
     await mainPage.setExtraHTTPHeaders({
         'Accept-Language': 'en-US,en;q=0.9,es-CL;q=0.8,es;q=0.7',
     });
-    await mainPage.goto('https://myanimelist.net/topanime.php?limit=1850', { timeout: 60000 });
+    await mainPage.goto('https://myanimelist.net/topanime.php?limit=1250', { timeout: 60000 });
     // await mainPage.screenshot({ path: './out/screenshot.png' });
     const animeList = await mainPage.$$('tr.ranking-list');
     await getData(browser, animeList);
@@ -45,55 +45,69 @@ const getData = async (browser, animeList) => {
                 'Accept-Language': 'en-US,en;q=0.9,es-CL;q=0.8,es;q=0.7',
             });
             await newPage.goto(url, { timeout: 60000 });
+
+            // if (
+            //     !document
+            //         .querySelectorAll('div.spaceit_pad')
+            //         .find((el) => el.innerText.includes('Demographic'))
+            //         ?.querySelector('a').innerText
+            // ) {
+            //     await newPage.close();
+            //     continue;
+            // }
+            try {
+                while (
+                    (await newPage.evaluate(() => {
+                        const el = document.querySelector('div.display-submit div.caption');
+                        if (el)
+                            return el.textContent.includes(
+                                'We are temporarily restricting site connections due to heavy access.',
+                            );
+                        else return false;
+                    })) &&
+                    (await newPage.$eval('button.g-recaptcha', () => true).catch(() => false))
+                ) {
+                    await Promise.all([
+                        newPage.waitForNavigation({ timeout: 60000 }),
+                        newPage.click('button.g-recaptcha'),
+                    ]);
+                    // await newPage.waitForSelector('h1.title-name.h1_bold_none strong',{ timeout: 0 });
+                }
+            } catch (err) {
+                console.log(err);
+                await newPage.screenshot({ path: './out/screenshot.png' });
+            }
+
             const data = await newPage.evaluate(() => document.querySelector('*').outerHTML);
             const document = HTMLParser.parse(data);
+
+            if (
+                document
+                    .querySelectorAll('div.spaceit_pad')
+                    .find((el) => el.innerText.includes('Genre'))
+                    ?.querySelectorAll('a')
+                    .map((element) => element.innerText).length === 0
+            ) {
+                await newPage.close();
+                continue;
+            }
+
             if (document.querySelector('span.information.type')?.textContent.includes('Music')) {
                 await newPage.close();
                 continue;
             }
-            while (
-                document
-                    .querySelector('div.display-submit div.caption')
-                    ?.textContent.includes('We are temporarily restricting site connections due to heavy access.') &&
-                (await newPage.$eval('button.g-recaptcha', () => true).catch(() => false))
-            ) {
-                await Promise.all([newPage.waitForNavigation({ timeout: 60000 }), newPage.click('button.g-recaptcha')]);
-                // await newPage.waitForSelector('h1.title-name.h1_bold_none strong',{ timeout: 0 });
-            }
-            console.log(document.querySelector('span.numbers.ranked strong').textContent);
+
+            console.log(await newPage.$eval('span.numbers.ranked strong', (node) => node.textContent));
+
             animesData.push({
                 title: document.querySelector('h1.title-name.h1_bold_none strong').textContent,
                 score: document.querySelector('div.score-label').textContent,
                 season: document.querySelector('span.information.season a')?.textContent ?? null,
-                synopsis: document.querySelector('p[itemprop="description"]').textContent,
-                characters:
-                    document.querySelector('div.detail-characters-list')?.previousElementSibling.querySelector('h2')
-                        .textContent === 'Characters & Voice Actors'
-                        ? document
-                              .querySelector('div.detail-characters-list')
-                              .querySelectorAll('table[width="100%"]')
-                              .map((character) => ({
-                                  character: {
-                                      name: character.querySelector('td.borderClass h3 a').textContent,
-                                      image: character
-                                          .querySelector('td.borderClass div.picSurround img')
-                                          .getAttribute('data-src'),
-                                  },
-                                  voiceActor: {
-                                      name: character.querySelector('td.va-t a')?.textContent ?? null,
-                                      image:
-                                          character
-                                              .querySelector('table div.picSurround img')
-                                              ?.getAttribute('data-src') ?? null,
-                                  },
-                              }))
-                        : null,
-                genres:
-                    document
-                        .querySelectorAll('div.spaceit_pad')
-                        .find((el) => el.innerText.includes('Genre'))
-                        ?.querySelectorAll('a')
-                        .map((element) => element.innerText) ?? null,
+                genres: document
+                    .querySelectorAll('div.spaceit_pad')
+                    .find((el) => el.innerText.includes('Genre'))
+                    ?.querySelectorAll('a')
+                    .map((element) => element.innerText),
                 demographic:
                     document
                         .querySelectorAll('div.spaceit_pad')
@@ -111,6 +125,7 @@ const getData = async (browser, animeList) => {
         const json = fs.readFileSync(filename);
         let animes = JSON.parse(json);
         animes = animes.concat(animesData);
+        console.log('Animes: ', animes.length);
         fs.writeFileSync(filename, JSON.stringify(animes));
     } else {
         fs.writeFileSync(filename, JSON.stringify(animesData));
